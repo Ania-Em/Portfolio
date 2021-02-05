8e4cdf99a38d2664c4afee72264b2799e73f1293
Ania-Em <aniaarchi@gmail.com>
2021-02-05 11:14:06 +0100

    Slides image working

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+
 function playmenu() {
-   var audio = new Audio("http://www.soundjay.com/switch/sounds/switch-13.mp3");
-   audio.play();
+  var audio = new Audio("http://www.soundjay.com/switch/sounds/switch-13.mp3");
+  audio.play();
 }
+
+const projectImage= document.querySelector(".project-image");
+const arrowsNext= document.querySelector(".arrow-next");
+const arrowsPrevious= document.querySelector(".arrow-previous");
+
+let numberOfImage = 1;
+
+arrowsNext.addEventListener("click", function(){projectImage.style.backgroundImage=`url('./assets/images/Dentico/${++numberOfImage}.png')`});
+
+arrowsPrevious.addEventListener("click", function(){projectImage.style.backgroundImage=`url('./assets/images/Dentico/${--numberOfImage}.png')`});
+
+
+
 
