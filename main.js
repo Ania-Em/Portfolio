b603394f9a0748adf39b761886e84ea777f812c3
Ania-Em <aniaarchi@gmail.com>
2021-02-06 18:51:01 +0100

    Working photo gallery

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-
 function playmenu() {
   var audio = new Audio("http://www.soundjay.com/switch/sounds/switch-13.mp3");
   audio.play();
 }
 
-const projectImage= document.querySelector(".project-image");
-const arrowsNext= document.querySelector(".arrow-next");
-const arrowsPrevious= document.querySelector(".arrow-previous");
+// Dentico
 
-let numberOfImage = 1;
+let firstNumber2 = 1;
+const projectImage2 = document.querySelector(".project2 .project-image");
+const arrowsNext2 = document.querySelector(".project2 .arrow-next");
+const arrowsPrevious2 = document.querySelector(".project2 .arrow-previous");
+let sliderActive2 = document.querySelector(".project2 .dot1")
+sliderActive2.classList.add("slide-active");
 
-arrowsNext.addEventListener("click", function(){projectImage.style.backgroundImage=`url('./assets/images/Dentico/${++numberOfImage}.png')`});
+arrowsNext2.addEventListener("click", function () {
+  if (firstNumber2 === 3){
+    firstNumber2 = 1;
+  } else {
+    ++firstNumber2;
+  } projectImage2.style.backgroundImage = `url('./assets/images/2/${firstNumber2}.png')`;
+  
+  let sliderActive2Previous = document.querySelector(".project2 .slide-active");
+  sliderActive2Previous.classList.remove("slide-active");
+  sliderActive2 = document.querySelector(`.project2 .dot${firstNumber2}`);
+  sliderActive2.classList.add("slide-active");
+});
 
-arrowsPrevious.addEventListener("click", function(){projectImage.style.backgroundImage=`url('./assets/images/Dentico/${--numberOfImage}.png')`});
+arrowsPrevious2.addEventListener("click", function () {
+  if (firstNumber2 === 1){
+    firstNumber2 = 3;
+  } else {
+    --firstNumber2;
+  }projectImage2.style.backgroundImage = `url('./assets/images/2/${firstNumber2}.png')`;
 
-
-
+  let sliderActive2Previous = document.querySelector(".project2 .slide-active");
+  sliderActive2Previous.classList.remove("slide-active");
+  sliderActive2 = document.querySelector(`.project2 .dot${firstNumber2}`);
+  sliderActive2.classList.add("slide-active");
+});
 
